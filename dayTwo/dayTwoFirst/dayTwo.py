2047d101e5e58b59382bcd88724415c08421a293
Axel Vandenabeele <axel.vandenabeele@epitech.eu>
2021-12-08 02:14:14 +0100

    dayTwoFirst

---
+++ dayTwo/dayTwoFirst/dayTwo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+file1 = open('../records_moves.txt', 'r')
+Lines = file1.readlines()
+
+abcissa = 0
+ordinate = 0
+
+for line in Lines:
+    line_dir = line.split(" ")
+    if line_dir[0] == "forward":
+        abcissa += int(line_dir[1])
+    elif line_dir[0] == "down":
+        ordinate += int(line_dir[1])
+    elif line_dir[0] == "up":
+        ordinate -= int(line_dir[1])
+
+print (abcissa * ordinate)
